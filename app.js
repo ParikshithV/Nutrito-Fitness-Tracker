c2590af8cbecc69be3360d707cb5ff56d9d80e0b
ParikshithV <Parikv99@gmail.com>
2020-07-07 04:25:19 +0530

    Migration
    Migtated from NutritoJS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var sha1 = require('sha1');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
@@ -79,40 +80,62 @@ function getData(){
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE IF NOT EXISTS IntakeData (InName VARCHAR(255), Protein VARCHAR(55), Carbohydrates VARCHAR(55), Fat VARCHAR(55), Calories VARCHAR(55))";
   var sql1 = "CREATE TABLE IF NOT EXISTS userdb (userName VARCHAR(255), email VARCHAR(255), age VARCHAR(15), height VARCHAR(55), weight VARCHAR(55), gender VARCHAR(55), password VARCHAR(255))";
-
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Done!");
-  })
 
   con.query(sql1, function (err, result) {
     if (err) throw err;
     console.log("Done!");
   })
-  getData();
 });
 
 app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/nutrito', function(req, res) {
   getData();
    res.cookie('sumPro', sumPro);
    res.cookie('sumCarbs', sumCarbs);
    res.cookie('sumFat', sumFat);
    res.cookie('sumCal', sumCal);
-    res.sendFile(path.join(__dirname + '/homepage.html'));
+   res.sendFile(path.join(__dirname + '/homepage.html'));
 });
 
 app.get('/about', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/signup', function(req, res) {
+app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname + '/loginPage.html'));
+});
+
+app.get('/register', function(req, res) {
     res.sendFile(path.join(__dirname + '/regPage.html'));
 });
 
-app.get('/loginPage', function(req, res) {
-    res.sendFile(path.join(__dirname + '/loginPage.html'));
+app.post('/loginSession', function(req, res) {
+    var userName = req.body.userName;
+    var password = req.body.password;
+    var password_en = sha1(password);
+    console.log("Login check...");
+
+    var sql = "SELECT * FROM userdb where userName = '"+userName+"' and password = '"+password_en+"'" ;
+    con.query(sql, function (error,  results) {
+    if (error) {
+   console.log("error ocurred",error);
+      }
+      else{
+            if(results.length >0)
+              {
+                res.redirect('nutrito');
+               }
+        else
+          {
+            res.cookie('resp', '300', {maxAge: 1000});
+            res.redirect('loginPage');
+          }
+        }
+      });
 });
 
 app.post('/save', urlencodedParser, function(req, res){
@@ -127,7 +150,7 @@ app.post('/save', urlencodedParser, function(req, res){
       if (err) throw err;
       console.log("input saved!");
     })
-  res.sendFile(path.join(__dirname + '/redirect.html'));
+  res.sendFile(path.join(__dirname + '/homepage.html'));
 })
 
 app.post('/signup', urlencodedParser, function(req, res){
@@ -139,24 +162,19 @@ app.post('/signup', urlencodedParser, function(req, res){
     var weight = req.body.userWeight;
     var gender = req.body.gender;
     var password = req.body.password;
-
+    var password_en = sha1(password);
     var sqlcheck = "select * from userdb where userName= '"+name+"'";
-    var sql = "insert into userdb (userName, email, age, height, weight, gender, password) values ('"+name+"','"+email+"','"+age+"','"+height+"','"+weight+"','"+gender+"','"+password+"')" ;
+    var sql = "insert into userdb (userName, email, age, height, weight, gender, password) values ('"+name+"','"+email+"','"+age+"','"+height+"','"+weight+"','"+gender+"','"+password_en+"')" ;
     var sqluser = "CREATE TABLE IF NOT EXISTS "+name+" (InName VARCHAR(255), Protein VARCHAR(55), Carbohydrates VARCHAR(55), Fat VARCHAR(55), Calories VARCHAR(55))" ;
 
     con.query(sqlcheck, function (err, rows, result) {
       if (err) throw err;
       console.log("UserName check");
-        // Object.keys(result).forEach(function(key) {
-        // var arr = JSON.stringify(rows[0]);
-        // var arred = JSON.parse(arr);
-        // console.log(arred["userName"]);
         if (rows.length > 0) {
           console.log("Username is already taken");
-          res.cookie('usernameCheck', 1);
-          res.sendFile(path.join(__dirname + '/regPage.html'));
+          res.cookie('resp', '300', {maxAge: 1000});
+          res.redirect('register');
         } else {
-          res.cookie('usernameCheck', 0);
           con.query(sql, function (err, result) {
             if (err) throw err;
             console.log("input saved!");
@@ -166,10 +184,12 @@ app.post('/signup', urlencodedParser, function(req, res){
             console.log("User registered");
           })
           console.log("Registration successful!");
-          res.sendFile(path.join(__dirname + '/loginPage.html'));
+          res.cookie('resp', '200', {maxAge: 1000});
+          res.redirect('register');
         }
   });
 })
+
 
 app.use(function(req, res, next) {
     res.status(404).send("It no work");
